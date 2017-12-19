7e3d4f2d9e217c0e20f4526ea93f4f5a026a9924
Idhrendur <idhrendur@hotmail.com>
2017-12-19 07:07:19 -0800

    Report name as according to province mappings

--- cityLocationScript.py
+++ cityLocationScript.py
@@ -18,6 +18,7 @@ locationsFile.close()
 provinceMappingsFile = open('province_mappings.txt', encoding='iso-8859-1')
 lines = [line.rstrip('\n') for line in provinceMappingsFile]
 provinceMappings = dict()
+provinceNames = dict()
 for line in lines:
 	if len(line) == 0: continue
 	if line[0] == '#': continue
@@ -25,11 +26,13 @@ for line in lines:
 	if (Vic2Match):
 		HoI4Matches = re.finditer("(hoi4 = )([0-9]+)", line)
 		if (HoI4Matches):
+			HoI4Name = re.search("# (.+)( ->)", line)
 			HoI4Provinces = []
 			for HoI4Match in HoI4Matches:
 				HoI4Provinces.append(HoI4Match.group(2))
 			for HoI4Province in HoI4Provinces:
 				provinceMappings[HoI4Province] = HoI4Provinces
+				provinceNames[HoI4Province] = HoI4Name.group(1)
 provinceMappingsFile.close()
 
 # Check city positions against mappings, report mismatches
@@ -37,6 +40,6 @@ outputFile = open('output.txt', "w+", encoding='iso-8859-1');
 for position in positionsList:
 	mapping = provinceMappings.get(position)
 	if mapping[0] != position:
-		outputString = "HoI4 province " + position + " was not first in its mapping! Locations name: " + positionsMap.get(position) + "\n"
+		outputString = "HoI4 province " + position + " was not first in its mapping! Locations name: " + positionsMap.get(position) + "\tMapping name: " + provinceNames.get(position) + "\n"
 		outputFile.write(outputString)
 outputFile.close();
