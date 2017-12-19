4468ff71330eca81e8f7767804d9ea42d8f3d0a0
Idhrendur <idhrendur@hotmail.com>
2017-12-18 19:26:47 -0800

    Read in the city positions

--- cityLocationScript.py
+++ cityLocationScript.py
@@ -4,8 +4,14 @@ import re
 
 locationsFile = open("cityLocations.txt", encoding='iso-8859-1')
 lines = [line.rstrip('\n') for line in locationsFile]
+positions = []
 for line in lines:
-	print(line)
+	positionMatch = re.search("([0-9]+)\t(.+)", line)
+	if (positionMatch):
+		positions.append(positionMatch.group(1))
 locationsFile.close()
+
+for position in positions:
+	print(position)
 
 input("Press enter to continue")
