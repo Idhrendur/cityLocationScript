8f7c7379025180aa85bd0031cddf3cdf15a3abf8
Idhrendur <idhrendur@hotmail.com>
2017-12-18 19:10:25 -0800

    Read in the city locations (gives an error on the print, but it's enough to verify lines are being read as lines. It won't matter soon).

--- cityLocationScript.py
+++ cityLocationScript.py
@@ -2,4 +2,10 @@
 # -*- coding: iso-8859-1 -*-
 import re
 
+locationsFile = open("cityLocations.txt", encoding='iso-8859-1')
+lines = [line.rstrip('\n') for line in locationsFile]
+for line in lines:
+	print(line)
+locationsFile.close()
+
 input("Press enter to continue")
